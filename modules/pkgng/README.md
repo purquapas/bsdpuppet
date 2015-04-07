f3cfe559b208121b420676260b8a687b249d4344
purquapas <olevole@yandex.ru>
2015-04-07 05:37:05 +0300

    wip

---
+++ modules/pkgng/README.md
@@ -0,0 +1,100 @@
+Puppet-pkgng
+===
+
+[![Build Status](https://travis-ci.org/xaque208/puppet-pkgng.png)](https://travis-ci.org/xaque208/puppet-pkgng)
+
+
+A package provider for FreeBSD's PkgNG package manager.
+
+This module contains the provider as well as some implementation around
+configuring the `pkg.conf` file.  If you are building your own PkgNG packages,
+you may also want to look at the [poudriere
+module](https://github.com/xaque208/puppet-poudriere).
+
+## Installation
+
+The easiest way to install is to install from the forge.
+
+```
+puppet module install zleslie/pkgng
+```
+
+Then to configure your system to use PkgNG with all default settings, a
+simple include will do.
+
+```Puppet
+include pkgng
+```
+
+You may wish to override settings though, to do this simply use it as a class:
+
+```Puppet
+class { 'pkgng':
+  purge_repos_d => false
+}
+```
+
+By default, this module will erase the default repository and you'll want to
+define some. To disable this behaviour you can use the example above of
+setting `purge_repos_d` to false.
+
+Using specific repositories is as simple as a Puppet resource.
+
+```Puppet
+pkgng::repo { 'pkg.freebsd.org': }  # You'll want this one!
+pkgng::repo { 'my.own.repo': }      # You can then specify more...
+```
+
+### Installation via [R10K](https://github.com/adrienthebo/r10k)
+
+You can also clone this repo to somewhere in your modulepath, or use something
+like [r10k](https://github.com/adrienthebo/r10k) to deploy your modules.  R10k
+is sweet.  For those not familiar, check out [Finch's blog
+post](http://somethingsinistral.net/blog/rethinking-puppet-deployment/) on the
+subject of module deployment.
+
+To track the git repository, a line in your Puppetfile that looks something
+like the following should get you started.
+
+```Ruby
+mod 'pkgng', :git => 'git://github.com/xaque208/puppet-pkgng.git', :ref => '0.2.0'
+```
+
+### Installation via [Librarian-Puppet](http://librarian-puppet.com/)
+
+Installation via Librarian-Puppet is straight forward, simply add the
+following to your `Puppetfile`
+
+```Ruby
+mod 'zleslie/pkgng'
+```
+
+## Usage
+
+Once you have the module installed, you can use it by simply adding a site
+default in site.pp that looks like this.
+
+```Puppet
+Package {
+  provider => pkgng
+}
+```
+
+Now every package that you install will use the PkgNG provider.
+
+With multiple repositories defined, a package can be installed from a specific
+repo by giving a URN locator for that repository like this.
+
+```Puppet
+pkgng::repo { 'my.own.repo': }
+
+package { 'puppet':
+  source => 'urn:freebsd:repo:my.own.repo',
+}
+```
+
+## Contributing
+
+Please help make this module better.  Send pull request, file issues, be
+mindful of the tests and help improve where you can.
+
---
+++ modules/pkgng/manifests/init.pp
@@ -0,0 +1,88 @@
+# This configures the PkgNG Package manager on FreeBSD systems, and adds
+# support for managing packages with Puppet.  This will eventually be in
+# mainline FreeBSD, but for now, we are leaving the installation up to the
+# adminstrator, since there is no going back.
+#
+# If you have purge_repos_d as true - then you'll have no repositories
+# defined unless you define one. You want to do this as you'll want this
+# module to control repos anyway.
+#
+# To install PkgNG, one can simply run the following:
+# make -C /usr/ports/ports-mgmg/pkg install clean
+class pkgng (
+  $pkg_dbdir     = $pkgng::params::pkg_dbdir,
+  $pkg_cachedir  = $pkgng::params::pkg_cachedir,
+  $portsdir      = $pkgng::params::portsdir,
+  $purge_repos_d = true,
+  $repos         = {},
+) inherits pkgng::params {
+
+  # PkgNG versions before 1.1.4 use another method of defining repositories
+  if ! $::pkgng_supported or versioncmp($::pkgng_version, '1.1.4') < 0 {
+    fail('PKGng is either not supported on your system or it is too old')
+  }
+
+  # Validate $purge_repos_d boolean
+  validate_bool($purge_repos_d)
+
+  file { '/usr/local/etc/pkg.conf':
+    content => "PKG_DBDIR: ${pkg_dbdir}\nPKG_CACHEDIR: ${pkg_cachedir}\nPORTSDIR: ${portsdir}\n",
+    notify  => Exec['pkg update'],
+  }
+
+  # make sure repo config dir is present
+  file { '/usr/local/etc/pkg':
+    ensure => directory,
+  }
+
+  if $purge_repos_d == true {
+    File['/usr/local/etc/pkg/repos'] {
+      recurse => true,
+      purge   => true,
+    }
+
+    file { '/etc/pkg':
+      ensure  => directory,
+      purge   => true,
+      recurse => true,
+      before  => Exec['pkg update']
+    }
+  }
+
+  file { '/usr/local/etc/pkg/repos':
+    ensure => directory,
+  }
+
+  file { '/etc/make.conf':
+    ensure => present,
+  }
+
+  file_line { 'WITH_PKGNG':
+    path    => '/etc/make.conf',
+    line    => "WITH_PKGNG=yes\n",
+    require => File['/etc/make.conf'],
+  }
+
+  # Triggered on config changes
+  exec { 'pkg update':
+    path        => '/usr/local/sbin',
+    refreshonly => true,
+    command     => 'pkg -q update -f',
+  }
+
+  # This exec should really on ever be run once, and only upon converting to
+  # pkgng. If you are building up a new system where the only software that
+  # has been installed form ports is the pkgng itself, then the pkg database
+  # is already up to date, and this is not required. As you will see,
+  # refreshonly, but nothing notifies this. I am uncertain at this time how
+  # to proceed, other than manually.
+  exec { 'convert pkg database to pkgng':
+    path        => '/usr/local/sbin',
+    refreshonly => true,
+    command     => 'pkg2ng',
+    require     => File['/etc/make.conf'],
+  }
+
+  # expand all pkg repositories from hashtable
+  create_resources('pkgng::repo', $repos)
+}
